b8c5c5bb25c439810879edc3cb7dde578c761e19
Banzai <bastian.norroy@insa-lyon.fr>
2018-03-28 17:56:17 +0200

    Faire le rangement des pions dans le tableau

--- LesDames.java
+++ LesDames.java
@@ -3,8 +3,8 @@ public class LesDames {
 	
 	public static void main (String[] args) {
 		Case plateau[][] = new Case[10][10];
-		Pion joueur1[] = new Pion[20];
-		Pion joueur2[] = new Pion[20];
+		//Pion joueur1[] = new Pion[20];
+		//Pion joueur2[] = new Pion[20];
 	
 	}
 	
@@ -14,7 +14,12 @@ public class LesDames {
 	}
 	
 	public Pion[] RemplirPlateau (int taille, int nbPion, boolean joueur1) {
-		
+		tableauPions[] = new tableauPions[];
+		for(int x = 0, x <= 10 , x++){
+			for(int y = 0, y <= 10, y++){
+				
+			}
+		}
 	return ;
 	}
 }
