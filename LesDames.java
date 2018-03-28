c95bcab91bf290231fc52f7a077445f6569e0132
Jean Ribes <jean@ribes.me>
2018-03-28 17:25:50 +0200

    création pions

--- LesDames.java
+++ LesDames.java
@@ -2,8 +2,9 @@
 public class LesDames {
 	
 	public static void main (String[] args) {
-		Case plateau[][] = new Case[];
-		
+		Case plateau[][] = new Case[10][10];
+		Pion joueur1[] = new Pion[10];
+		Pion joueur2[] = new Pion[10];
 	}
 }
 
