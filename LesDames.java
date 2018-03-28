b7dcd63833bd605d68caac090638eadabb316919
Banzai <bastian.norroy@insa-lyon.fr>
2018-03-28 17:44:08 +0200

    Découpage fonctionnel

--- LesDames.java
+++ LesDames.java
@@ -3,8 +3,18 @@ public class LesDames {
 	
 	public static void main (String[] args) {
 		Case plateau[][] = new Case[10][10];
-		Pion joueur1[] = new Pion[10];
-		Pion joueur2[] = new Pion[10];
+		Pion joueur1[] = new Pion[20];
+		Pion joueur2[] = new Pion[20];
+	
+	}
+	
+	public Case[][] CreerPlateau (int taille){
+	
+	return ;
+	}
+	
+	public Pion[] RemplirPlateau (int taille, int nbPion, boolean joueur1) {
+		
+	return ;
 	}
 }
-
